61df0c35d08db0868632edec32ffa25c0c923561
Samal <evgeniy.samal@mail.ru>
2022-11-30 16:26:47 +0300

    Вывел данные о погоде в нужном для меня городе (Речица).

--- weathermap/main/views.py
+++ weathermap/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+import requests
 def index(request):
-    return render(request,'main/index.html')
+    appid = '7f0d775f3fa2bd1bb790b2ec36dee211'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
+
+    city = 'Rechytsa'
+    res = requests.get(url.format(city))
+
+    print(res.text)
+    return render(request, 'main/index.html')
 
 def new(request):
     return render(request,'main/new.html')
