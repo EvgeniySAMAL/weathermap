d762e54f57296afcee951a87005cae41c376d78d
Samal <evgeniy.samal@mail.ru>
2022-12-01 12:14:16 +0300

    Создал новый шаблон (new.html)  для отображения прогноза погоды

--- weathermap/main/views.py
+++ weathermap/main/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
 import requests
+
 def index(request):
     appid = '7f0d775f3fa2bd1bb790b2ec36dee211'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
 
     city = 'Rechytsa'
-    res = requests.get(url.format(city))
+    res = requests.get(url.format(city)).json()
 
-    print(res.text)
-    return render(request, 'main/index.html')
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+    }
+    context = {
+        'info': city_info
+    }
+    return render(request, 'main/index.html',context)
 
 def new(request):
-    return render(request,'main/new.html')
+
+    appid = '7f0d775f3fa2bd1bb790b2ec36dee211'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+    }
+    context = {
+        'info': city_info
+    }
+    return render(request, 'main/new.html', context)
